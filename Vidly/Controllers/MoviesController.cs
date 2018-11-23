c2725eb0db4e7d372f76df0819311a8fb3b6648e
Saad <saadmasood96@hotmail.com>
2018-11-23 01:08:29 -0500

    First few database migrations

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+       
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "A Quiet Place"},
+                new Movie { Id = 1, Name = "Ex Machina"}
+            };
+        }
+
+        [Route("movies/released/{year}/{month:range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+
+            return Content(year + "/"+month);
+        }
+    }
+}
